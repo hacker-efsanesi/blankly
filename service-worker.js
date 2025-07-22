7d873e5d458869bf4f81c6585863b09303c56ba3
hacker-efsanesi <mindmate60@gmail.com>
2025-07-22 13:38:10 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('blankly-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
